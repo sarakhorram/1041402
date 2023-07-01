dc6ed4450f399626a66590365616ee88554c251c
sarakhorram <138280783+sarakhorram@users.noreply.github.com>
2023-07-01 10:36:11 +0330

    Add files via upload

---
+++ Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	public float ali;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position += new Vector3 (0,0,0);
+
+		transform.eulerAngles += new Vector3 (0,0,0);
+
+		transform.localScale += new Vector3 (0,0,0);
+
+	}
+}
